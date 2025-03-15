09e9b8183580c29df1dffdbeede871f41b6e7b52
muratcan sakin <lluxysa@gmail.com>
2025-03-15 22:37:46 +0300

    Added db connection

--- src/Infrastructure/AppDbContext.cs
+++ src/Infrastructure/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace Infrastructure
 {
-    class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
     }
 }
