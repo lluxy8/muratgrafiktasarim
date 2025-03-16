83a3906afc3a99e3811f04b19e91bb88441b1a16
muratcan sakin <lluxysa@gmail.com>
2025-03-16 10:35:59 +0300

    Added unit of work

--- src/Infrastructure/UnitOfWork.cs
+++ src/Infrastructure/UnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,59 @@ using System.Threading.Tasks;
 
 namespace Infrastructure
 {
-    class UnitOfWork
+    public sealed class UnitOfWork
     {
+        private readonly AppDbContext _context;
+
+        private IDbContextTransaction _transaction;
+        public AdminRepository AdminRepository { get; }
+        public CategoryRepository CategoryRepository { get; }
+        public ProjectRepository ProjectRepository { get; }
+
+        public UnitOfWork(AppDbContext context, IUserRepository userRepository, IProductRepository product)
+        {
+            _context = context;
+            UserRepository = userRepository;
+            ProductRepository = product;
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            if (_transaction == null)
+                _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            if (_transaction == null)
+                throw new InvalidOperationException("Transaction not started.");
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                await _transaction.CommitAsync();
+            }
+            catch (Exception)
+            {
+                await RollbackTransactionAsync();
+                throw;
+            }
+
+        }
+
+        public void Dispose()
+        {
+            _transaction?.Dispose();
+            _context.Dispose();
+        }
+
+        public async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
+
+        public async Task RollbackTransactionAsync()
+        {
+            if (_transaction == null)
+                throw new InvalidOperationException("Transaction not started.");
+
+            await _transaction.RollbackAsync();
+        }
     }
-}
