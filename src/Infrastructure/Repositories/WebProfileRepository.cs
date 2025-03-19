c8ac7805e12fc3d2f0902125683f0b36eb24a82d
muratcan sakin <lluxysa@gmail.com>
2025-03-19 18:41:41 +0300

    Added webprofilerepository

---
+++ src/Infrastructure/Repositories/WebProfileRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public sealed class WebProfileRepository : GenericRepository<WebProfile>
+    {
+        public WebProfileRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
